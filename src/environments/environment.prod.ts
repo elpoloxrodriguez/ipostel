5169caef2f183e8b7d840b31a35eb9dc9f8d4441
elpoloxrodriguez <elpoloxrodriguez@gmail.com>
2023-03-07 11:30:59 -0400

    Entorno de Prueba Ipostel subdominio SIRPRUEBA.IPOSTEL.GOB.VE 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  Url: 'https://localhost',
+  Url: 'https://sirprueba.ipostel.gob.ve',
   API: '/v1/api/',
   hmr: false,
   ID : 'ID-001',
-  apiUrl: 'https://localhost',
+  apiUrl: 'https://sirprueba.ipostel.gob.ve',
   Hash: ':c521f27fb1b3311d686d511b668e5bd4'
 };
